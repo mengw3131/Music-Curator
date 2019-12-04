a945d321d93ef913dbea5ba2fbffbfbb22103681
lukecglover <lukecglover@gmail.com>
2019-12-04 18:06:43 -0500

    Raised the recommendation limits, reduced repeated recommendations

--- src/main/java/com/curator/controllers/SurveyController.java
+++ src/main/java/com/curator/controllers/SurveyController.java
@@ -1,10 +1,19 @@
 package com.curator.controllers;
 
-import com.curator.models.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+
+import com.curator.models.Artist;
+import com.curator.models.Genre;
+import com.curator.models.Track;
 import com.curator.tools.DBTools;
 import com.curator.tools.RecTools;
 import com.curator.tools.SpotifyTools;
 import com.curator.views.SurveyButton;
+
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,146 +26,139 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.*;
-
 public class SurveyController implements Initializable {
 
-    @FXML
-    private Button okButton;
+	@FXML
+	private Button okButton;
 
-    @FXML
-    private FlowPane genreFlowPane;
+	@FXML
+	private FlowPane genreFlowPane;
 
-    @FXML
-    private FlowPane artistFlowPane;
+	@FXML
+	private FlowPane artistFlowPane;
 
-    @FXML
-    private FlowPane trackFlowPane;
+	@FXML
+	private FlowPane trackFlowPane;
 
-    private Stage stage;
+	private Stage stage;
 
-    @FXML
-    private Label genreQuestionLabel;
+	@FXML
+	private Label genreQuestionLabel;
 
-    @FXML
-    private Label artistQuestionLabel;
+	@FXML
+	private Label artistQuestionLabel;
 
-    @FXML
-    private Label trackQuestionLabel;
+	@FXML
+	private Label trackQuestionLabel;
 
-    @FXML
-    private Label welcomeLabel;
+	@FXML
+	private Label welcomeLabel;
 
+	private HashSet<String> uniqueArtistsID = new HashSet<String>();
+	private HashSet<String> uniqueTracksID = new HashSet<String>();
 
-    private HashSet<String> uniqueArtistsID = new HashSet<String>();
-    private HashSet<String> uniqueTracksID = new HashSet<String>();
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		welcomeLabel.setText("Hi, " + DBTools.getUserId() + "!");
 
+		okButton.setVisible(false);
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        welcomeLabel.setText("Hi, " + DBTools.getUserId() + "!");
+		populateGenre();
 
-        okButton.setVisible(false);
+		okButton.setOnMousePressed(new EventHandler<>() {
+			@Override
+			public void handle(MouseEvent event) {
+				System.out.println("Storing your picks");
+				storePicks();
 
-        populateGenre();
+				// Run recommender
+				System.out.println("Curating recommendation...");
+				RecTools.runCombinedRecommender();
+//                RecTools.runAlbumRecommender();
+//                RecTools.runArtistRecommender();
+//                RecTools.runSongRecommender();
+				System.out.println("Fetching data...");
 
-        okButton.setOnMousePressed(new EventHandler<>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("Storing your picks");
-                storePicks();
+				FXMLLoader loader = new FXMLLoader(
+						getClass().getResource("/views/main.fxml"));
+				try {
+					stage.setScene(new Scene(loader.load()));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 
-                //Run recommender
-                System.out.println("Curating recommendation...");
-//                RecTools.runCombinedRecommender();
-                RecTools.runAlbumRecommender();
-                RecTools.runArtistRecommender();
-                RecTools.runSongRecommender();
-                System.out.println("Fetching data...");
+				stage.setHeight(600);
+				stage.setMinWidth(1300);
+				stage.setTitle("Music Curator");
+				stage.show();
+			}
+		});
+	}
 
+	public void storePicks() {
+		for (Node node : artistFlowPane.getChildren()) {
+			SurveyButton btn = (SurveyButton) node;
+			if (btn.isSelected()) {
+				DBTools.storeUserPreferenceArtist(btn.getItemId(), true);
+			}
+		}
 
+		for (Node node : trackFlowPane.getChildren()) {
+			SurveyButton btn = (SurveyButton) node;
+			if (btn.isSelected()) {
+				DBTools.storeUserPreferenceTracks(btn.getItemId(), true);
 
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/main.fxml"));
-                try {
-                    stage.setScene(new Scene(loader.load()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+				// store track's album
+				DBTools.storeUserPreferenceAlbum(SpotifyTools
+						.getTrack(btn.getItemId()).getAlbum().getAlbumID(),
+						true);
+			}
+		}
+	}
 
-                stage.setHeight(600);
-                stage.setMinWidth(1300);
-                stage.setTitle("Music Curator");
-                stage.show();
-            }
-        });
-    }
+	public void populateGenre() {
+		for (Genre genre : Genre.values()) {
+			genreFlowPane.getChildren().add(
+					new SurveyButton(genre.id, genreFlowPane, this, 0, ""));
+		}
+	}
 
-    public void storePicks() {
-        for (Node node : artistFlowPane.getChildren()) {
-            SurveyButton btn = (SurveyButton) node;
-            if (btn.isSelected()) {
-                DBTools.storeUserPreferenceArtist(btn.getItemId(), true);
-            }
-        }
+	public void populateArtist(ArrayList<Artist> artists) {
+		for (Artist artist : artists) {
+			if (!uniqueArtistsID.contains(artist.getArtistID())) {
+				uniqueArtistsID.add(artist.getArtistID());
+				artistFlowPane.getChildren()
+						.add(new SurveyButton(artist.getName(), artistFlowPane,
+								this, 1, artist.getArtistID()));
+			}
+		}
+	}
 
-        for (Node node : trackFlowPane.getChildren()) {
-            SurveyButton btn = (SurveyButton) node;
-            if (btn.isSelected()) {
-                DBTools.storeUserPreferenceTracks(btn.getItemId(), true);
+	public void populateTrack(ArrayList<Track> tracks) {
+		for (Track track : tracks) {
+			if (!uniqueTracksID.contains(track.getTrackID())) {
+				uniqueTracksID.add(track.getTrackID());
+				trackFlowPane.getChildren()
+						.add(new SurveyButton(track.getTrackName(),
+								trackFlowPane, this, 2, track.getTrackID()));
+			}
+		}
+	}
 
-                //store track's album
-                DBTools.storeUserPreferenceAlbum(
-                        SpotifyTools.getTrack(btn.getItemId()).getAlbum().getAlbumID(), true);
-            }
-        }
-    }
+	public void showArtistQuestionLabel() {
+		artistQuestionLabel.setOpacity(1);
 
-    public void populateGenre() {
-        for (Genre genre : Genre.values()) {
-            genreFlowPane.getChildren().add(new SurveyButton(genre.id,
-                    genreFlowPane, this, 0, ""));
-        }
-    }
+	}
 
-    public void populateArtist(ArrayList<Artist> artists) {
-        for (Artist artist : artists) {
-            if (!uniqueArtistsID.contains(artist.getArtistID())) {
-                uniqueArtistsID.add(artist.getArtistID());
-                artistFlowPane.getChildren().add(
-                        new SurveyButton(artist.getName(), artistFlowPane, this, 1, artist.getArtistID()));
-            }
-        }
-    }
+	public void showTrackQuestionLabel() {
+		trackQuestionLabel.setOpacity(1);
+	}
 
-    public void populateTrack(ArrayList<Track> tracks) {
-        for (Track track : tracks) {
-            if (!uniqueTracksID.contains(track.getTrackID())) {
-                uniqueTracksID.add(track.getTrackID());
-                trackFlowPane.getChildren().add(
-                        new SurveyButton(track.getTrackName(),
-                                trackFlowPane, this, 2, track.getTrackID()));
-            }
-        }
-    }
+	public void showOkButton() {
+		okButton.setVisible(true);
+	}
 
-
-    public void showArtistQuestionLabel(){
-        artistQuestionLabel.setOpacity(1);
-
-    }
-
-    public void showTrackQuestionLabel(){
-        trackQuestionLabel.setOpacity(1);
-    }
-
-    public void showOkButton(){
-        okButton.setVisible(true);
-    }
-
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
 }
-
--- src/main/java/com/curator/tools/RecTools.java
+++ src/main/java/com/curator/tools/RecTools.java
@@ -81,8 +81,8 @@ public class RecTools {
 	public static void runCombinedRecommender() {
 		boolean newUser = true;
 		SongRecommender s = new SongRecommender();
-//		new ArtistRecommender(s.getUserRecs(), newUser);
-//		new AlbumRecommender(s.getUserRecs(), newUser);
+		new ArtistRecommender(s.getUserRecs(), newUser);
+		new AlbumRecommender(s.getUserRecs(), newUser);
 	}
 
 	/**
@@ -185,7 +185,7 @@ public class RecTools {
 	public static Track popTrack() {
 		checkAndReplenishTrack();
 		Track t = null;
-		if (trackQueue.size() != 0){
+		if (trackQueue.size() != 0) {
 			t = trackQueue.get(0);
 			trackQueue.remove(0);
 			trackQueueSize--;
@@ -199,7 +199,7 @@ public class RecTools {
 	public static Album popAlbum() {
 		checkAndReplenishAlbum();
 		Album a = null;
-		if (albumQueue.size() != 0){
+		if (albumQueue.size() != 0) {
 			a = albumQueue.get(0);
 			albumQueue.remove(0);
 			albumQueueSize--;
